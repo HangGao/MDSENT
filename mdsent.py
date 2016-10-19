7ea45b97a2b1dcc5179465b5b741dc5e74795b09
hanggao <hanggao1@umbc.edu>
2016-10-18 20:51:15 -0400

    bug fix

--- mdsent.py
+++ mdsent.py
@@ -145,8 +145,8 @@ class Model(object):
         set_all_param_values(self.network, param_values)
 
     def save(self, file_path):
-        print self.parameters
-        print get_all_param_values(self.network)
+        self.get_params()
+        print 'okay!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
         pickle.dump((self.parameters, self.get_params()), open(file_path, 'wb'))
 
     @staticmethod
